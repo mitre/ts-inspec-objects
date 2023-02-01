8d3e8aa7312385f35f0f3c384c16611bccdd72a3
Pavan Kumar Bondalapati <pbondalapati@mitre.org>
2023-02-01 09:17:54 -0500

    Handle describe block test cases

---
+++ test/tests/describe.spec.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import Control from '../../src/objects/control'
+import {getExistingDescribeFromControl} from '../../src/index'
+
+describe('describe block extraction', () => {
+  const pathToTestCases = 'test/sample_data/describe-test-cases'
+  let files = fs.readdirSync(pathToTestCases)
+  files = files.map(x => x.slice(0, x.length - 3)) // Remove the '.rb' from file names
+  test.each(files)('case involving %s', (file) => {
+    let expectedOutput = '  describe_block = nil' // Default expected output
+    const controlCode = fs.readFileSync(`${pathToTestCases}/${file}.rb`, 'utf-8')
+    const testControl = new Control({code: controlCode})
+    const generatedOutput = getExistingDescribeFromControl(testControl)
+    
+    // Redefine variable `expectedOutput` for customized expected output, accordingly.
+    // Tip: use an escaped string for readability (e.g., https://www.browserling.com/tools/add-slashes).
+    if (file == 'comments') {
+      expectedOutput = '  # Commodo sed egestas egestas fringilla.\n  # Ultricies tristique nulla aliquet enim. \n  describe_block = nil\n  # Volutpat consequat mauris nunc congue nisi.\n=begin  Pellentesque sit amet porttitor eget. Duis at tellus at urna. Pretium aenean  pharetra magna ac placerat vestibulum lectus mauris ultrices. Bibendum at  varius vel pharetra vel turpis nunc eget lorem. Ultrices mi tempus imperdiet  nulla malesuada pellentesque elit eget gravida. =end\n\n  # This comment is in the describe block.\n\n=begin This is a multi-line comment in the describe block.  Vestibulum lorem sed risus ultricies tristique nulla. Interdum velit euismod  in pellentesque massa. Et magnis dis parturient montes nascetur ridiculus mus  mauris vitae. Augue lacus viverra vitae congue eu. Et ultrices neque ornare  aenean. Lectus urna duis convallis convallis tellus id interdum velit. =end\n\n  describe_block = nil'
+    } else if (file == 'headers-in-describe') {
+      expectedOutput = '  describe_block = nil\n  if describe_block\n    impact 1.0\n    tag \'headers\': \'in describe\'\n    ref \'https://sample.com\'\n    desc \'Amet dictum sit amet justo.\'\n  end'
+    }
+    expect(expectedOutput).toEqual(generatedOutput);
+  });
+});
