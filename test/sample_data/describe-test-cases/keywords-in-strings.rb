5d54b05cc553c8b34560f11537c87148e696abb9
Will Dower <wdower@mitre.org>
2023-02-02 11:39:37 -0500

    fixed whitespacing in test case for keywords in strings
    Signed-off-by: Will Dower <wdower@mitre.org>

--- test/sample_data/describe-test-cases/keywords-in-strings.rb
+++ test/sample_data/describe-test-cases/keywords-in-strings.rb
@@ -1,12 +1,12 @@
 control 'keywords-in-strings' do
-    title 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+  title 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
   eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   impact 0.5
   tag 'End on': 'desc'
   ref 'https://example.com'
   desc 'Enim lobortis scelerisque fermentum dui faucibus.'
 
-  keyword_in_string = 'Lorem ipsum desc test control  title'
+  keyword_in_string = 'Lorem ipsum desc test control'
   describe_block = nil
 end
   
