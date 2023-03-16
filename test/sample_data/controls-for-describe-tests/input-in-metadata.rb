8ce9c5e8c75f50d61f2de5865f05885366742227
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2023-03-16 15:26:55 -0500

    add test for having inputs in the metadata
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

---
+++ test/sample_data/controls-for-describe-tests/input-in-metadata.rb
@@ -0,0 +1,10 @@
+control 'input-in-metadata' do
+    title 'Lorem #{input('org_name')[:acronym]} dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    impact 0.5
+    tag 'End on': 'desc'
+    ref 'https://example.com'
+    desc 'Enim lobortis scelerisque fermentum dui faucibus.'
+  
+    describe_block = nil
+end
