4b29c06a29e1d263f916d2d3052de7079f362e7f
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2023-03-16 16:01:19 -0500

    change to correct quote for inputs
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- test/sample_data/controls-for-describe-tests/input-in-metadata.rb
+++ test/sample_data/controls-for-describe-tests/input-in-metadata.rb
@@ -1,6 +1,6 @@
 control 'input-in-metadata' do
-    title 'Lorem #{input('org_name')[:acronym]} dolor sit amet, consectetur adipiscing elit, sed do 
-    eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    title "Lorem #{input('org_name')[:acronym]} dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua."
     impact 0.5
     tag 'End on': 'desc'
     ref 'https://example.com'
