9a6ab1e8dcd79a16cb8d9736be2a3ff27b6ca27b
Amndeep Singh Mann <amann@mitre.org>
2026-02-27 13:26:58 -0500

    update eslint to match what we're gonna do in saf cli
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/parsers/xccdf.ts
+++ src/parsers/xccdf.ts
@@ -1,6 +1,6 @@
 import { prettify } from 'htmlfy';
 import _ from 'lodash';
-import { data as CciNistMappingData } from '../mappings/cci-nist-mapping-data';
+import { data as CciNistMappingData } from '../mappings/cci_nist_mapping_data';
 import Control from '../objects/control';
 import Profile from '../objects/profile';
 import { OvalDefinitionValue } from '../types/oval';
