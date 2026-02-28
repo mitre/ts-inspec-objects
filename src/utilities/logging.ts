c48fc1d4befac219b7817fda0aac96adebacbd14
Amndeep Singh Mann <amann@mitre.org>
2026-02-28 01:32:49 -0500

    further improvements to the logger + resolve some linter issues
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/utilities/logging.ts
+++ src/utilities/logging.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import { createLogger, format, transports, type Logger } from 'winston';
 
 // Use user defined colors. Used by the console log transporter
@@ -21,16 +22,23 @@ export function createWinstonLogger(library = 'inspec-objects', level = 'debug')
     level: level,
     transports: [new transports.Console()],
     format: format.combine(
-      format.simple(),
       format.timestamp({
         format: 'MMM-DD-YYYY HH:mm:ss Z',
       }),
       format.errors({ stack: true }),
       format.printf(
         (info) => {
-          const prefix = colorizer.colorize('prefix', `[${[info.timestamp]} -> ${library}]:`);
-          const msg = colorizer.colorize(info.level, JSON.stringify(info.stack, null, 2) || JSON.stringify(info.message, null, 2) || '');
-          return `${prefix} ${msg}`;
+          const prefix = colorizer.colorize('prefix', `[${info.timestamp as string} -> ${library}]`);
+          const lvl = colorizer.colorize(info.level, info.level);
+          let msg = '';
+          if (info.message) {
+            msg = _.isString(info.message) ? info.message : JSON.stringify(info.message, null, 2);
+          }
+          if (info.stack) {
+            msg += `\n${_.isString(info.stack) ? info.stack : JSON.stringify(info.stack, null, 2)}`;
+          }
+          msg = colorizer.colorize(info.level, msg);
+          return `${prefix} - ${lvl}: ${msg}`;
         },
       ),
     ),
